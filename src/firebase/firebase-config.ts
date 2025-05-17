2ce30dcf37cd2164f6c4e9b43a91ca2aaae6afcf
Thien Dev <nguyenvthiendev@gmail.com>
2025-05-17 13:02:42 +0700

    feat: update chat in call modal

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -1,22 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDQhDcQIrL-abmVcmDwSWqIIiQnSiQprOI',
-  authDomain: 'talktodoc-6c9b0.firebaseapp.com',
-  databaseURL:
-    'https://talktodoc-6c9b0-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'talktodoc-6c9b0',
-  storageBucket: 'talktodoc-6c9b0.firebasestorage.app',
-  messagingSenderId: '399631715856',
-  appId: '1:399631715856:web:d444ad4f2bfbc52e894495'
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
